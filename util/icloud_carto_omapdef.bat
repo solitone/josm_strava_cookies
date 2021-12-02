0b0091fc9bc8d3ce5bda8961789049440dad1452
solitone <solitone@mail.com>
2021-12-02 17:47:21 +0100

    Removed folder 'util'.

--- util/icloud_carto_omapdef.bat
+++
@@ -1,1 +0,0 @@
-python3 ..\carto_strava_omapdef.py > carto_strava.onlinemap
--- util/read_strava_cookies.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Key-Pair-Id: "
-python3 ../BinaryCookieReader.py ~/Library/Cookies/Cookies.binarycookies |grep CloudFront-Key-Pair-Id|cut -d"=" -f2|cut -d";" -f1
-echo "Policy: "
-python3 ../BinaryCookieReader.py ~/Library/Cookies/Cookies.binarycookies |grep CloudFront-Policy|cut -d"=" -f2|cut -d";" -f1
-echo "Signature: "
-python3 ../BinaryCookieReader.py ~/Library/Cookies/Cookies.binarycookies |grep CloudFront-Signature|cut -d"=" -f2|cut -d";" -f1
